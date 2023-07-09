c5e94bd298d3ae5b5ac7fa3235da9c79d28d5ddb
Mawuli Akpey <akpeym@gmail.com>
2023-07-09 08:50:12 +0000

    Update hun_first.py

--- hun_first.py
+++ hun_first.py
@@ -21,7 +21,7 @@ st.title("Stock Price Predictor")
 st.write("This application predicts stock prices using a trained neural network model.")
 
 # List of tickers used for building the model
-tickers = ['AAPL', 'GOOGL', 'MSFT', 'AMZN']
+tickers = ['AAPL', 'MSFT', 'JNJ', 'PFE', 'XOM', 'CVX']
 
 # Create a dropdown menu for ticker selection
 ticker = st.selectbox('Select a stock ticker', tickers)
