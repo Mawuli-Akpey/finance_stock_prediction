c0925c8b6c032dde5ffa355245e676e9bebb2bff
Mawuli Akpey <akpeym@gmail.com>
2023-07-21 11:29:41 +0000

    Update hun_first.py

--- hun_first.py
+++ hun_first.py
@@ -42,6 +42,12 @@ def most_recent_weekday():
     most_recent = today - datetime.timedelta(offset)
     return most_recent
 
+# Define a dictionary to map month names to numbers
+month_to_number = {
+    'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6,
+    'July': 7, 'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12
+}
+
 # Start of the Streamlit app
 st.write("""
 # Stock Closing Price Prediction App
@@ -50,19 +56,24 @@ Warning: This app is for informational purposes only. Please make your financial
 
 # Ask the user to input the year, month, and day
 year = st.number_input('Enter a year', min_value=2023, max_value=2030, value=2023, step=1)
-month = st.number_input('Enter a month', min_value=1, max_value=12, value=1, step=1)
-day = st.number_input('Enter a day', min_value=1, max_value=31, value=1, step=1)
+month = st.selectbox('Select a month', list(month_to_number.keys()))
+day = st.date_input('Select a day')
 
 # When the 'Predict' button is clicked, make the prediction
 if st.button('Predict'):
     # The start date (the date of the last known closing price)
     start_date = most_recent_weekday()
 
-    # The future date (the date for which the user wants to predict the closing price)
-    future_date = datetime.date(year, month, day)
+    # Convert the month from text to number
+    month_number = month_to_number[month]
 
-    # Ensure future_date is after start_date
-    if future_date <= start_date:
+    # The future date (the date for which the user wants to predict the closing price)
+    future_date = datetime.date(year, month_number, day.day)
+
+    # Check if the future date is a weekday
+    if future_date.weekday() >= 5:  # 0-4 denotes Monday-Friday
+        st.write('Error: The future date must be a weekday.')
+    elif future_date <= start_date:
         st.write('Error: The future date must be after the most recent weekday.')
     else:
         # The initial sequence of the most recent known closing prices
